07b1ad4da7f48331b40934ebff121564d1c91f6b
Roma <romavb@iclou.com>
2022-03-27 04:41:23 +0200

    components and video

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
+
+import Post from '../../components/Post';
 
 const Home = () => {
   return (
     <View>
-      <Text> Home</Text>
+      <Post />
     </View>
   );
 };
